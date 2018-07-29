182bbbb59f2f524da7e43bc857c1320f56fb958e
Mitch Shelton <>
2018-07-28 23:41:58 -0600

    removed starter contetnt

---
+++ Source/BuildingEscape/PositionRerport.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionRerport.h"
+
+
+// Sets default values for this component's properties
+UPositionRerport::UPositionRerport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	
+}
+
+
+// Called when the game starts
+void UPositionRerport::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s."), *ObjectName, *ObjectPos);
+	
+}
+
+
+// Called every frame
+void UPositionRerport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
